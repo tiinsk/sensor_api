b63afdf9e5ac19c6348181948e642134f1e6999f
tiinsk <tiina.koskiranta@gmail.com>
2019-04-24 12:51:52 -0700

    Add location_type and name to device

---
+++ knex/migrations/20190424124458_add_name_and_location_type_to_device.js
@@ -0,0 +1,18 @@
+exports.up = async function(knex, Promise) {
+
+  await knex.schema.table('device', function (table) {
+    table.string('name');
+    table.enu('location_type', ['outside', 'inside'], { useNative: true, enumName: 'location_type' });
+  });
+
+};
+
+exports.down = async function(knex, Promise) {
+  await knex.schema.table('device', function (table) {
+    table.dropColumn('name');
+    table.dropColumn('location_type');
+  });
+  await knex.schema.raw(`
+    DROP TYPE location_type;
+  `);
+};
